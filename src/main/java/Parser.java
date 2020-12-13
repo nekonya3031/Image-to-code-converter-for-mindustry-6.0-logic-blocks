b17d1932ff81a62484027bf2eaa7783e0ac521c0
_owler_#0954 <63801085+owler0954@users.noreply.github.com>
2020-12-13 11:16:41 +0300

    Готовый генератор схем чек

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,12 +1,35 @@
+import arc.struct.Seq;
+import arc.struct.StringMap;
+import mindustry.Vars;
+import mindustry.core.ContentLoader;
+import mindustry.core.Version;
+import mindustry.ctype.Content;
+import mindustry.ctype.ContentType;
+import mindustry.game.Schematic;
+import mindustry.game.Schematics;
+import mindustry.world.Block;
+
 import java.util.ArrayList;
 import java.util.*;
-import java.awt.image.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.*;
+import java.util.zip.DeflaterOutputStream;
 
+//todo фикс у коорды. фикс зависимостей
 public class Parser {
     public static void main(String[] args) {
+        Version.enabled = false;
+        Vars.content = new ContentLoader();
+        Vars.content.createBaseContent();
+        for (ContentType type : ContentType.values()) {
+            for (Content content : Vars.content.getBy(type)) {
+                try {
+                    content.init();
+                } catch (Throwable ignored) {
+                }
+            }
+        }
         try {
             String baka;
             FileWriter writer0 = new FileWriter("output0.txt", false);
@@ -16,7 +39,7 @@ public class Parser {
             FileWriter writer4 = new FileWriter("output4.txt", false);
             FileWriter writer5 = new FileWriter("output5.txt", false);
 
-            ArrayList<Rect> rects =new ArrayList<>();
+            ArrayList<Rect> rects = new ArrayList<>();
             Set<Integer>colors=new HashSet<Integer>();
 
             System.out.print("p");
@@ -113,8 +136,9 @@ while(x<w){
             writer1.flush();
             writer3.flush();
             System.out.println("end standart actions set");
-            
+
             String[][] sch=new String[8][8];
+            sch[0][0] = "";
             int maxColor=getBGcolor(rects,colors);
             rects=rmColor(maxColor,rects);
             int schx=0;
@@ -147,13 +171,17 @@ while(x<w){
                         //TODO killer remove
                         //if(PFChecker>10){return;}
                         writerC.flush();
-                        writerC = new FileWriter("COut"+PFChecker+".txt", false);
+                        writerC = new FileWriter("COut" + PFChecker + ".txt", false);
                         writerC.append(TColor);
-                        sch[schx][schy]+=TColor;
+                        sch[schx][schy] = "";
+                        sch[schx][schy] += TColor;
                     }
                 }
             }
             writerC.flush();
+            writer5.append(generate(sch));
+            writer5.flush();
+
             System.out.println("end custom actions set");
 
         } catch (IOException e) {
@@ -162,14 +190,16 @@ while(x<w){
         ;
 
     }
+
     public static ArrayList<Rect> getColor(int c, ArrayList<Rect> a){
         ArrayList<Rect> rtn=new ArrayList<>();
         for(Rect r:a){
             if(r.rgb==c){
-            rtn.add(r);
-        }}
+                rtn.add(r);
+            }}
         return rtn;
     }
+
     public static ArrayList<Rect> rmColor(int c, ArrayList<Rect> a){
         ArrayList<Rect> rtn=new ArrayList<>();
         for(Rect r:a){
@@ -178,31 +208,90 @@ while(x<w){
             }}
         return rtn;
     }
-    public static Integer getBGcolor(ArrayList<Rect> a,Set<Integer> c){
+
+    public static Integer getBGcolor(ArrayList<Rect> a, Set<Integer> c){
         int rtn=0;
         int maxWal=0;
         int tempWal=0;
         for(int e:c){
-            tempWal=0;
-        for(Rect r:a){
-            if(r.rgb==e){
-                tempWal++;
+            tempWal = 0;
+            for (Rect r : a) {
+                if (r.rgb == e) {
+                    tempWal++;
+                }
             }
-        }
-        if(tempWal>maxWal){maxWal=tempWal;rtn=e;}
+            if (tempWal > maxWal) {
+                maxWal = tempWal;
+                rtn = e;
+            }
         }
         return rtn;
     }
+
+    public static String generate(String[][] c) {
+        String sirterSch = "bXNjaAF4nGNgZGBkYmDJS8xNZeC4sP9iw4V9F9sYuFNSi5OLMgtKMvPzGBgYGfhzM5OL8nULivKTU4uL84sYQIIgwAfE3BVzklMamBgYhBiYGADInhTo";
+        Block lp = null;
+        Schematic sorterschema = Schematics.readBase64(sirterSch);
+        for (Schematic.Stile gg : sorterschema.tiles) {
+            lp = gg.block;
+        }
+        if (lp == null) {
+            System.out.println("ERROR SYKA");
+        }
+        int a = 0;
+        int b;
+        byte f = 0;
+        Seq<Schematic.Stile> tiles = new Seq<>();
+        while (a < 8) {
+            b = 0;
+            while (b < 7) {
+                if (c[a][b] != null) {
+                    tiles.add(new Schematic.Stile(lp, a, b, compress(c[a][b]), f));
+                }
+                b++;
+            }
+            a++;
+        }
+        StringMap tags = new StringMap();
+        tags.put("name", "photo");
+        Schematic schem = new Schematic(tiles, tags, 8, 8);
+
+        Schematics aaa = new Schematics();
+        aaa.saveChanges(schem);
+        return aaa.writeBase64(schem);
+    }
+
+    static byte[] compress(String code) {
+        return compress(code.getBytes(Vars.charset));
+    }
+
+    static byte[] compress(byte[] bytes) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream stream = new DataOutputStream(new DeflaterOutputStream(baos));
+            stream.write(1);
+            stream.writeInt(bytes.length);
+            stream.write(bytes);
+            int actives = 0;
+            stream.writeInt(actives);
+            stream.close();
+            return baos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
-class Rect{
+
+class Rect {
     int x;
     int y;
     int z;
     int rgb;
-    Rect(int x,int y,int z,int rgb){
-        this.x=x;
-        this.y=y;
-        this.z=z;
+
+    Rect(int x, int y, int z, int rgb) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
         this.rgb=rgb;
     }
 }
