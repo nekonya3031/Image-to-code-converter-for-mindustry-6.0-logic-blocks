df688d148c58dd45ccbce5645fe722660c42741b
_owler_#0954 <63801085+owler0954@users.noreply.github.com>
2020-12-13 11:36:58 +0300

    Очистка мусора и неактуал кода

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -32,23 +32,23 @@ public class Parser {
         }
         try {
             String baka;
-            FileWriter writer0 = new FileWriter("output0.txt", false);
-            FileWriter writer1 = new FileWriter("output1.txt", false);
-            FileWriter writer2 = new FileWriter("output2.txt", false);
-            FileWriter writer3 = new FileWriter("output3.txt", false);
-            FileWriter writer4 = new FileWriter("output4.txt", false);
-            FileWriter writer5 = new FileWriter("output5.txt", false);
+
+            FileWriter writer = new FileWriter("schematic.txt", false);
 
             ArrayList<Rect> rects = new ArrayList<>();
-            Set<Integer>colors=new HashSet<Integer>();
+            Set<Integer> colors = new HashSet<Integer>();
 
-            System.out.print("p");
+            System.out.print("Starting load picture");
             BufferedImage bi = ImageIO.read(new File("image.png"));
 
             int w = bi.getWidth();
-            if(w>176){w=176;}
+            if (w > 176) {
+                w = 176;
+            }
             int h = bi.getHeight();
-            if(h>176){h=176;}
+            if (h > 176) {
+                h = 176;
+            }
             int[][] pictureR = new int[w][h];
             int[][] pictureG = new int[w][h];
             int[][] pictureB = new int[w][h];
@@ -57,7 +57,7 @@ public class Parser {
 
             int x = 0;
             int y = 0;
-            System.out.println("pp");
+            System.out.println("picture loading succsess");
             while (x < w) {
                 y = 0;
                 while (y < h) {
@@ -70,7 +70,7 @@ public class Parser {
                 }
                 x++;
             }
-            System.out.println("ppa");
+            System.out.println("picture decoding to base colors succsess");
             x = 0;
             int lastY = 0;
             int counter = 1;
@@ -95,92 +95,62 @@ public class Parser {
                 counter = 1;
                 x++;
             }
-            System.out.println("ppe");
-            x = 0;
-            int printer = 0;
+            System.out.println("graphic primetives finding succsess");
 
-            while (x < w) {
-                y = 0;
-                while (y < h) {
-                    if (yChecker[x][y] != -1) {
-                        printer++;
-                        writer1.append("draw color " + pictureR[x][y] + " " + pictureG[x][y] + " " + pictureB[x][y] + " 255 0 0\ndraw rect " + x + " " + y + " 1 " + (yChecker[x][y] + 1) + " 0 0\n");
-                        writer0.append(" " + yChecker[x][y] + " ");
-                        rects.add(new Rect(x,y,yChecker[x][y] + 1,bi.getRGB(x, y)));
-                    } else
-                        writer0.append(yChecker[x][y] + " ");
-                    if (printer > 100) {
-                        printer = 0;
-                        writer1.append("drawflush display1\n");
-                    }
-                    y++;
-                }
-                x++;
-                writer0.append("\n");
-            }
-            writer1.append("drawflush display1");
-/*
-x=0;
-while(x<w){
-    y=0;
-    while(y<h){
-        if(pictureFh1[x][y]!=-1){writer1.append(" "+pictureFh1[x][y]+" ");}else
-        writer1.append(pictureFh1[x][y]+" ");
-        y++;
-    }
-    x++;writer1.append("\n");
-}
 
- */
-            writer0.flush();
-            writer1.flush();
-            writer3.flush();
-            System.out.println("end standart actions set");
-
-            String[][] sch=new String[8][8];
+            String[][] sch = new String[8][8];
             sch[0][0] = "";
-            int maxColor=getBGcolor(rects,colors);
-            rects=rmColor(maxColor,rects);
-            int schx=0;
-            int schy=0;
-            int stringChecker=2;
-            int PFChecker=1;
-            String TColor=null;
-            FileWriter writerC = new FileWriter("COut"+PFChecker+".txt", false);
-            writerC.append("draw clear " + ((maxColor >> 16) & 0xFF) + " " + ((maxColor >> 8) & 0xFF) + " " + ((maxColor) & 0xFF) + " 255 0 0\n");
-            sch[schx][schy]+="draw clear " + ((maxColor >> 16) & 0xFF) + " " + ((maxColor >> 8) & 0xFF) + " " + ((maxColor) & 0xFF) + " 255 0 0\n";
-            for(int c:colors){
-                TColor=("draw color " + ((c >> 16) & 0xFF) + " " + ((c >> 8) & 0xFF) + " " + ((c) & 0xFF) + " 255 0 0\n");
+            int maxColor = getBGcolor(rects, colors);
+            rects = rmColor(maxColor, rects);
+            int schx = 0;
+            int schy = 0;
+            int stringChecker = 2;
+            int PFChecker = 1;
+            String TColor = null;
+            //FileWriter writerC = new FileWriter("COut"+PFChecker+".txt", false);
+            //writerC.append("draw clear " + ((maxColor >> 16) & 0xFF) + " " + ((maxColor >> 8) & 0xFF) + " " + ((maxColor) & 0xFF) + " 255 0 0\n");
+            sch[schx][schy] += "draw clear " + ((maxColor >> 16) & 0xFF) + " " + ((maxColor >> 8) & 0xFF) + " " + ((maxColor) & 0xFF) + " 255 0 0\n";
+            for (int c : colors) {
+                TColor = ("draw color " + ((c >> 16) & 0xFF) + " " + ((c >> 8) & 0xFF) + " " + ((c) & 0xFF) + " 255 0 0\n");
                 stringChecker++;
-                writerC.append(TColor);
-                sch[schx][schy]+=TColor;
-                for(Rect r:getColor(c,rects)){
-                    writerC.append("draw rect " + r.x + " " + r.y + " 1 " + r.z + " 0 0\n");
-                    sch[schx][schy]+="draw rect " + r.x + " " + r.y + " 1 " + r.z + " 0 0\n";
+                //writerC.append(TColor);
+                sch[schx][schy] += TColor;
+                for (Rect r : getColor(c, rects)) {
+                    sch[schx][schy] += "draw rect " + r.x + " " + r.y + " 1 " + r.z + " 0 0\n";
                     stringChecker++;
-                    if(stringChecker==250||stringChecker==500||stringChecker==750){
-                        writerC.append("drawflush display1\n");stringChecker++;
-                        sch[schx][schy]+="drawflush display1\n";
+                    if (stringChecker == 250 || stringChecker == 500 || stringChecker == 750) {
+                        stringChecker++;
+                        sch[schx][schy] += "drawflush display1\n";
                     }
-                    if(stringChecker==1000||stringChecker>999){
-                        writerC.append("drawflush display1");stringChecker=2;
-                        sch[schx][schy]+="drawflush display1";
+                    if (stringChecker == 999 && schx == 0 && schy == 0) {
+                        stringChecker = 2;
+                        sch[schx][schy] += "drawflush display1\nend";
                         PFChecker++;
                         schx++;
-                        if(schx>7){schy++;schx=0;}
-                        //TODO killer remove
-                        //if(PFChecker>10){return;}
-                        writerC.flush();
-                        writerC = new FileWriter("COut" + PFChecker + ".txt", false);
-                        writerC.append(TColor);
+                        if (schx > 7) {
+                            schy++;
+                            schx = 0;
+                        }
+                        sch[schx][schy] = "";
+                        sch[schx][schy] += TColor;
+                    }
+                    if (stringChecker == 1000 || stringChecker > 999) {
+                        stringChecker = 2;
+                        sch[schx][schy] += "drawflush display1";
+                        PFChecker++;
+                        schx++;
+                        if (schx > 7) {
+                            schy++;
+                            schx = 0;
+                        }
                         sch[schx][schy] = "";
                         sch[schx][schy] += TColor;
                     }
                 }
             }
-            writerC.flush();
-            writer5.append(generate(sch));
-            writer5.flush();
+
+            writer.append(generate(sch));
+            writer.flush();
 
             System.out.println("end custom actions set");
 
